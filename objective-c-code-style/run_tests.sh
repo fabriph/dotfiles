e1a249e7ec2d0d196cc887aeeda89b11a204d71e
Fab <fabriph@users.noreply.github.com>
2015-01-26 18:54:06 -0300

    Script for running tests.

---
+++ objective-c-code-style/run_tests.sh
@@ -0,0 +1,13 @@
+TMP_OUTPUT="test.styleobjc.tmp"
+
+runWithFile () {
+  echo -e "\n$1\n"
+  ./styleobjc.sh "./test/$1" > $TMP_OUTPUT
+  diff $TMP_OUTPUT "./test/expected-$1"
+}
+
+runWithFile "colon-spacing.txt"
+runWithFile "math-operands.txt"
+runWithFile "method-signature.txt"
+runWithFile "pointers.txt"
+
