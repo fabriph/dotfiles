5b60db7ba5da28fa4319fda11aa8fab0b223a97f
Fab <fabriph@users.noreply.github.com>
2015-01-30 12:11:38 -0300

    Adding Shell script with RegEx for code formating.

---
+++ objective-c-code-style/run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TMP_OUTPUT="test.styleobjc.tmp"
+
+runWithFile () {
+  echo -e "\n$1\n"
+  ./styleobjc.sh "./test/$1" > $TMP_OUTPUT
+ # TODO:What diff tool is better? diff, git diff, colordiff?
+  git diff $TMP_OUTPUT "./test/expected-$1"
+}
+
+runWithFile "colon-spacing.txt"
+runWithFile "math-operands.txt"
+runWithFile "method-signature.txt"
+runWithFile "pointers.txt"
+
---
+++ objective-c-code-style/styleobjc.sh
@@ -0,0 +1,63 @@
+# Simple code syntax for Objective C.
+
+# TODO: accept many files as parameters
+# TODO: show help if non entries, or if -h or --help
+# TODO: default is not inplace, unles -i
+# TODO: option -d --diff to show differences with original file
+
+TMP_FILE='temp.syntax-style'
+
+cat $1 > $TMP_FILE
+
+# TODO: use with precaution, this script might edit character inside strings.
+# sed -i '' "s,,,g" $TMP_FILE
+
+
+## GENERAL
+# TODO: this can be better done by analyzing different scenarios.
+# Removing spaces around ':'
+sed -i '' "s,\([^ 	]\)[ 	]*:,\1:,g" $TMP_FILE
+sed -i '' "s,:[ 	]*,:,g" $TMP_FILE
+
+
+## OPERANDS
+# Spaces around '=' and '=='
+sed -i '' "s;\([^ 	]\)[ 	]*\([=]\{1,\}\)[ 	]*\([^ 	]\);\1 \2 \3;g" $TMP_FILE
+
+
+## POINTERS
+# Removes right spaces.
+sed -i '' "s-\*[ 	]*-\*-" $TMP_FILE
+# Normalizes left spaces.
+sed -i '' "s-\([^(][^ 	]*\)[ 	]*\*\([^)]\)-\1 \*\2-" $TMP_FILE
+
+
+## METHOD DECLARATION
+# Normalizes spaces in method return type: '   -   (void)    method' -> '- (void)method'
+sed -i '' "s,^[ 	]*-[ 	]*(\([^)]*\))[ 	]*,- (\1)," $TMP_FILE
+# Normalize spaces after colon in method signature
+sed -i '' "s,\(^[ 	]*-.*\)[ 	]*:[ 	]*\(.*[ 	]*{\),\1:\2,g" $TMP_FILE
+# Normalize spaces after closing parentesis for parameter tipe: "call:(type) var" -> "call:(type)var"
+sed -i '' "s,\(^[ 	]*-.*:[ 	]*([^)]*)\)[ 	]*\(.*[ 	]*{\),\1\2,g" $TMP_FILE
+# Exactly one space before opening { on method declaration
+sed -i '' "s,\(^[ 	]*-.*[^ 	]\)[ 	]*{,\1 {," $TMP_FILE
+
+
+## METHOD CALLING
+# Removing spaces after [ and before ]
+sed -i '' "s,^\([ 	]*\[\)[ 	]*\([^ 	].*[^ 	]\)[ 	]*\\(].*\)$,\1\2\3,g" $TMP_FILE
+# Removing double spacing
+sed -i '' "s;^\(.*\[.*\)[ 	]\{2,\};\1 ;g" $TMP_FILE
+sed -i '' "s;^\([ 	]*[^ 	].*\)[ 	]\{2,\};\1 ;g" $TMP_FILE
+
+
+## REVERTS (Putting spaces back when suitable)
+# Ternary operand like 'condition ? positive : negative'
+sed -i '' "s,\([^ 	]\)[ 	]*\?[ 	]*\(.*\)[ 	]*:[ 	]*\(.*\)$,\1 ? \2 : \3,g" $TMP_FILE
+# Enums like: 'typedef enum : Type {'
+sed -i '' "s;^\([ 	]*\)typedef[ 	]\{1,\}enum[ 	]*:[ 	]*\([^ 	]*\)[ 	]*{$;\1typedef enum : \2 {;" $TMP_FILE
+
+cat $TMP_FILE
+
+rm $TMP_FILE
+
