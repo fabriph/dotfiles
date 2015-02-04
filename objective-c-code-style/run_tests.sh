47fbd4cdcfa813afcb84cb0576ab299d433c43cd
Fab <fabriph@users.noreply.github.com>
2015-02-04 10:36:20 -0300

    Using colordiff for debugging.

--- objective-c-code-style/run_tests.sh
+++ objective-c-code-style/run_tests.sh
@@ -5,7 +5,8 @@ runWithFile () {
   echo -e "\n$1\n"
   ./styleobjc.sh "./test/$1" > $TMP_OUTPUT
  # TODO:What diff tool is better? diff, git diff, colordiff?
-  git diff $TMP_OUTPUT "./test/expected-$1"
+  colordiff $TMP_OUTPUT "./test/expected-$1"
+  rm "$TMP_OUTPUT"
 }
 
 runWithFile "colon-spacing.txt"
