3cf780da67e27dea0e1689908c29d8e1eacad011
Fab <fabriph@users.noreply.github.com>
2015-02-10 12:49:37 -0300

    Init.

---
+++ objective-c-code-style/README.md
@@ -0,0 +1,2 @@
+A script for styling Objective C code.
+
---
+++ objective-c-code-style/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TMP_OUTPUT="test.styleobjc.tmp"
+
+runWithFile () {
+  echo -e "\n$1\n"
+  ./styleobjc.sh "./test/$1" > $TMP_OUTPUT
+ # TODO:What diff tool is better? diff, git diff, colordiff?
+  colordiff $TMP_OUTPUT "./test/expected-$1"
+  rm "$TMP_OUTPUT"
+}
+
+runWithFile "colon-spacing.txt"
+runWithFile "bool-operands.txt"
+runWithFile "method-signature.txt"
+runWithFile "pointers.txt"
+runWithFile "general-code.txt"
+
