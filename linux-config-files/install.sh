13d1e47fd4136546a7acd374d1d1152700f8ba95
Fab <fabriph@users.noreply.github.com>
2015-05-24 01:04:58 -0300

    General improvements.

--- linux-config-files/install.sh
+++ linux-config-files/install.sh
@@ -11,19 +11,19 @@ function handle_backup {
         REPLY=""
         while [[ ! $REPLY =~ ^[YyNn]$ ]]
         do
-            read -p "Backup exists, override? (Y/n)" -n 1 -r
+            read -p "  Backup exists, override? (Y/n)" -n 1 -r
             echo
         done
         if [[ $REPLY =~ ^[Nn]$ ]]
         then
-            echo "  Backup Skipped"
+            echo "    Backup Skipped"
             return
         else
             rm -f "$destination.backup"
         fi
     fi
     mv "$destination" "$destination.backup"
-    echo "  Backup: $destination.backup"
+    echo "    Backup: $destination.backup"
 }
 
 function install_package {
@@ -39,7 +39,7 @@ function install_package {
         done
         if [[ $REPLY =~ ^[Ss]$ ]]
         then
-            echo "  Skipped"
+            echo -e "    Skipped\n"
             return
         fi
         if [[ $REPLY =~ ^[Bb]$ ]]
@@ -47,13 +47,18 @@ function install_package {
             handle_backup $destination
         fi
     fi
-    echo -e "$package installed. $destination\n"
     ln -s "$origin" "$destination"
+    echo -e "    Successfully installed\n"
 }
 
 install_package "Bash Profile" ~/dev/scripts/linux-config-files/bash_profile ~/.bash_profile
+
 install_package "VIM config file" ~/dev/scripts/linux-config-files/vimrc ~/.vimrc
+
 install_package "GIT Prompt" ~/dev/scripts/linux-config-files/git/git-prompt.sh ~/.git-prompt.sh
+
 install_package "GIT Completition" ~/dev/scripts/linux-config-files/git/git-completion.bash ~/.git-completion.bash
+
 install_package "Sublime config" ~/dev/scripts/linux-config-files/sublime/settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+
 install_package "Sublime keyboard" ~/dev/scripts/linux-config-files/sublime/keyboard ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
