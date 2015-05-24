1e4d9ba2b0b8ae841bad8b935389e4bc5327b4db
Fab <fabriph@users.noreply.github.com>
2015-05-24 01:08:01 -0300

    TODOs solved.

--- linux-config-files/install.sh
+++ linux-config-files/install.sh
@@ -2,8 +2,7 @@
 #  - Be able to install it as a remote curl, something like curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
 #  - If system is OSX use some naming, otherwise use regular Linux naming (example .bash_profile showuld be .profile).
 #  - use pwd + local references so we dont need to always checkout this repo on ~/dev/scripts.
-#  - If files are already there, ask if wanna remplace them.
-#  - Add the option to keep old files and then easily restore them, just in case I wanna use this install.sh as a temporary fix on somebody else computer.
+#  - Option to restore backup files.
 
 function rmsoft {
     TRASH_DIR=~/.tmp-trash
