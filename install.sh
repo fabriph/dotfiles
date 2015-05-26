0dc3bfaa013e40c98b1b6872f0bc2cc338adf09e
Fab <fabriph@users.noreply.github.com>
2015-05-26 13:34:33 -0300

    Moving stuff upwards for a cleaner repository.

--- install.sh
+++ install.sh
@@ -64,14 +64,14 @@ function install_package {
 
 today=`date +%Y-%m-%d.%H:%M:%S`
 
-install_package "Bash Profile" ~/dev/scripts/linux-config-files/bash_profile ~/.bash_profile
+install_package "Bash Profile" ~/dev/scripts/bash_profile ~/.bash_profile
 
-install_package "VIM config file" ~/dev/scripts/linux-config-files/vimrc ~/.vimrc
+install_package "VIM config file" ~/dev/scripts/vimrc ~/.vimrc
 
-install_package "GIT Prompt" ~/dev/scripts/linux-config-files/git/git-prompt.sh ~/.git-prompt.sh
+install_package "GIT Prompt" ~/dev/scripts/git/git-prompt.sh ~/.git-prompt.sh
 
-install_package "GIT Completition" ~/dev/scripts/linux-config-files/git/git-completion.bash ~/.git-completion.bash
+install_package "GIT Completition" ~/dev/scripts/git/git-completion.bash ~/.git-completion.bash
 
-install_package "Sublime config" ~/dev/scripts/linux-config-files/sublime/settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+install_package "Sublime config" ~/dev/scripts/sublime/settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
-install_package "Sublime keyboard" ~/dev/scripts/linux-config-files/sublime/keyboard ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
+install_package "Sublime keyboard" ~/dev/scripts/sublime/keyboard ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
