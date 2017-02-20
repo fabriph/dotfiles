a563a820d5313238ea02f4cc43d6e9dc5fce63b2
Fab <fabriph@users.noreply.github.com>
2017-02-19 22:13:03 -0500

    Code health

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Warning: this file is versioned in Github !!!
+
 # This file doesn't hold any special black magic. It's prefferably to use
 # bashrc for most of your configurations in case you SSH into your computer.
 
